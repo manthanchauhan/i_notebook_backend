001bbbd4dce0b844c73b45f7a2cbde7af4d55e19
manthan <manthanchauhan@indiagold.co>
2023-04-29 13:16:19 +0530

    added custom error handling

---
+++ src/main/java/com/example/i_notebook_backend/utils/ApiError.java
@@ -0,0 +1,19 @@
+package com.example.i_notebook_backend.utils;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Setter
+public class ApiError {
+
+    private HttpStatus status;
+    private String message;
+
+    public ApiError(HttpStatus status, String message) {
+        super();
+        this.status = status;
+        this.message = message;
+    }
+}
---
+++ src/main/java/com/example/i_notebook_backend/utils/ApiExceptionHandler.java
@@ -0,0 +1,54 @@
+package com.example.i_notebook_backend.utils;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.List;
+
+@ControllerAdvice
+// https://www.baeldung.com/global-error-handler-in-a-spring-rest-api
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest request) {
+        String errorMessage = this.extractErrorMessageMethodArgumentNotValid(ex);
+
+        ApiError apiError =
+                new ApiError(HttpStatus.BAD_REQUEST, errorMessage);
+        return handleExceptionInternal(
+                ex, apiError, headers, apiError.getStatus(), request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMissingServletRequestParameter(
+            MissingServletRequestParameterException ex, HttpHeaders headers,
+            HttpStatusCode status, WebRequest request) {
+        ApiError apiError =
+                new ApiError(HttpStatus.BAD_REQUEST, ex.getLocalizedMessage());
+        return new ResponseEntity<>(
+                apiError, new HttpHeaders(), apiError.getStatus());
+    }
+
+    private String extractErrorMessageMethodArgumentNotValid(MethodArgumentNotValidException ex){
+        List<ObjectError> errorList = ex.getBindingResult().getAllErrors();
+        String errorMessage = null;
+
+        if (!errorList.isEmpty()){
+            ObjectError firstError = errorList.get(0);
+            errorMessage = firstError.getDefaultMessage();
+        }
+
+        return errorMessage == null ? "Something went wrong" : errorMessage;
+    }
+}
