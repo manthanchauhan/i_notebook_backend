a76ced44b8f2699edea6178a18baf7184204ae3a
manthan <manthanchauhan@indiagold.co>
2023-04-29 20:29:30 +0530

    Fixed ApiErrorSerializer using jackson

--- src/main/java/com/example/i_notebook_backend/utils/errorHandling/ApiError.java
+++ src/main/java/com/example/i_notebook_backend/utils/errorHandling/ApiError.java
@@ -1,13 +1,11 @@
 package com.example.i_notebook_backend.utils.errorHandling;
 
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.http.HttpStatus;
 
 @Getter
 @Setter
-@JsonSerialize(using = ApiErrorSerializer.class)
 public class ApiError {
 
     private HttpStatus status;
--- src/main/java/com/example/i_notebook_backend/utils/errorHandling/ApiErrorSerializer.java
+++ src/main/java/com/example/i_notebook_backend/utils/errorHandling/ApiErrorSerializer.java
@@ -1,16 +1,15 @@
 package com.example.i_notebook_backend.utils.errorHandling;
 
 import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
-import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+import org.springframework.boot.jackson.JsonComponent;
 
 import java.io.IOException;
 
-public class ApiErrorSerializer extends StdSerializer<ApiError> {
-    public ApiErrorSerializer(Class<ApiError> t) {
-        super(t);
-    }
-
+@JsonComponent
+public class ApiErrorSerializer extends JsonSerializer<ApiError> {
+    // https://www.baeldung.com/spring-boot-jsoncomponent
     @Override
     public void serialize(ApiError apiError, JsonGenerator jgen, SerializerProvider provider) throws IOException {
 
